fd86ba4eb2336d69bd7080666ea99af570fb6d5e
Garima <garimajain120403@gmail.com>
2025-05-01 15:06:03 +0530

    first commit

---
+++ app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Navbar from "@/components/home/navbar";
+
+export default function Home() {
+  return (
+   <div>
+    <Navbar />
+   </div>
+  );
+}
